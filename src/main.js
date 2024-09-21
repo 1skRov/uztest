ce09b1dea8e02b4a3cf4aa91dc777c33f475dded
SarVar <sarvarrozikulov9@gmail.com>
2024-09-21 22:19:33 +0500

    Initial commit

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import './assets/index.css';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(autoAnimatePlugin).mount('#app')
