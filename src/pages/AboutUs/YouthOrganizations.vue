1d89c9e77c1da68b73b83593f52979ef3e83d278
SarVar <sarvarrozikulov9@gmail.com>
2024-11-06 17:22:54 +0500

    upd

--- src/pages/AboutUs/YouthOrganizations.vue
+++ src/pages/AboutUs/YouthOrganizations.vue
@@ -1,6 +1,20 @@
 <script>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/swiper-bundle.css';
+import SwiperCore, { Navigation } from 'swiper';
+import PrevBtn from "@/components/buttons/prevBtn.vue";
+import NextBtn from "@/components/buttons/nextBtn.vue";
+
+SwiperCore.use([Navigation]);
+
 export default {
   name: "YouthOrganizations",
+  components: {
+    NextBtn,
+    PrevBtn,
+    Swiper,
+    SwiperSlide,
+  },
   props:{
     data:{
       type: Object,
@@ -14,19 +28,35 @@ export default {
         require('@/assets/images/img.png'),
         require('@/assets/images/img.png'),
         require('@/assets/images/img.png'),
-      ]
+      ],
+      swiperInstance: null,
     }
-  }
+  },
+  methods: {
+    goNext() {
+      if (this.swiperInstance) {
+        this.swiperInstance.slideNext();
+      }
+    },
+    goPrev() {
+      if (this.swiperInstance) {
+        this.swiperInstance.slidePrev();
+      }
+    },
+    setSwiperInstance(swiper) {
+      this.swiperInstance = swiper;
+    },
+  },
 }
 </script>
 
 <template>
-  <div class="w-full main">
+  <div class="main">
     <h2 class="font-gilroy">
       {{data.category_title}}
     </h2>
     <div class="gallery">
-      <div class="row">
+      <div class="row-1">
         <div
             class="gallery-item"
             v-for="(image, index) in images.slice(0, 2)"
@@ -35,8 +65,8 @@ export default {
           <div class="overlay-container">
             <img :src="image" alt="Изображение">
             <div class="overlay">
-              <span class="font-gilroy text-start mb-4" style="font-size: 24px; font-weight: 500; color: white">{{ data.title }}</span>
-              <span class="text-white mt-2 text-start mb-6 font-normal" style="font-size: 14px">
+              <span class="font-gilroy title">{{ data.title }}</span>
+              <span class="desc">
                 {{ data.mini_desc }}
               </span>
               <div>
@@ -49,7 +79,7 @@ export default {
           </div>
         </div>
       </div>
-      <div class="row">
+      <div class="row-2">
         <div
             class="gallery-item"
             v-for="(image, index) in images.slice(2, 5)"
@@ -58,14 +88,13 @@ export default {
           <div class="overlay-container">
             <img :src="image" alt="Изображение">
             <div class="overlay">
-              <span class="font-gilroy text-start mb-4" style="font-size: 24px; font-weight: 500; color: white">{{ data.title }} test</span>
-              <span class="text-white mt-2 text-start mb-6 font-normal" style="font-size: 14px">
-              {{ data.mini_desc }} - test
-            </span>
+              <span class="font-gilroy title">{{ data.title }}</span>
+              <span class="desc">{{ data.mini_desc }}</span>
               <div>
                 <router-link to="/organization-details">
-                  <button class="uppercase font-medium text-base py-4 px-6 bg-white" style="border-radius: 6px; border: 1px solid #CFD3DA">
-                    {{ data.buttons_title }}</button>
+                  <button class="uppercase font-medium text-base py-4 px-6 bg-white mt-1.5" style="border-radius: 6px; border: 1px solid #CFD3DA">
+                    {{ data.buttons_title }}
+                  </button>
                 </router-link>
               </div>
             </div>
@@ -73,81 +102,228 @@ export default {
         </div>
       </div>
     </div>
+    <div class="gallery-mob">
+      <div>
+        <swiper
+            :slides-per-view="1"
+            class="mySwiper"
+            @swiper="setSwiperInstance"
+        >
+          <swiper-slide v-for="(image, index) in images" :key="index">
+            <div class="gallery-cont">
+              <img :src="image" alt="Изображение" class="carousel-image" />
+              <span class="font-gilroy title">{{ data.title }}</span>
+              <span class="desc">{{ data.mini_desc }}</span>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
 
+      <div class="items-center flex justify-center mt-4 gap-10">
+        <prev-btn @click="goPrev" class="z-10"/>
+        <next-btn @click="goNext"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
-.overlay-container {
-  position: relative;
-  overflow: hidden;
-}
-
-.overlay-container img {
-  display: block;
-  width: 100%;
-  height: auto;
-}
-
-.overlay {
-  @apply flex flex-col absolute top-0 left-0 p-4;
-  justify-content: flex-end;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 114, 171, 0.7);
-  opacity: 0;
-  transition: opacity 0.5s ease;
-}
-
-.overlay-text {
-  color: white;
-  font-size: 18px; /* Измените размер шрифта по необходимости */
-  font-weight: 500; /* Измените, чтобы подстроить под ваши требования */
-  text-align: center;
-}
-
-.overlay-container:hover .overlay {
-  opacity: 1;
-}
-.gallery {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
-
-.gallery .row {
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  margin-bottom: 15px;
-}
-
-.gallery .gallery-item {
-  flex: 1;
-  margin: 0 5px;
-}
-
-.gallery .gallery-item img {
-  width: 100%;
-  height: auto;
-}
 .main {
   padding: 0 0 80px;
-
   h2 {
-    font-weight: 500; font-size:40px;
+    font-weight: 500;
+    font-size: 40px;
+    margin-bottom: 12px;
+  }
+  .gallery {
+    @apply flex flex-col items-center;
+    .row-1 {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      margin-bottom: 15px;
+
+      .gallery-item {
+        flex: 1;
+        margin: 0 5px;
+
+        img {
+          width: 100%;
+          height: auto;
+        }
+        .overlay-container {
+          position: relative;
+          overflow: hidden;
+          img {
+            display: block;
+            width: 100%;
+            height: auto;
+          }
+          .overlay {
+            @apply flex flex-col absolute top-0 left-0 p-4;
+            justify-content: flex-end;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 114, 171, 0.7);
+            opacity: 0;
+            transition: opacity 0.5s ease;
+            .title {
+              @apply text-start mb-1;
+              font-size: 24px; font-weight: 500; color: white;
+            }
+            .desc {
+              @apply text-white mt-2 text-start mb-6 font-normal;
+              font-size: 14px;
+              display: -webkit-box;
+              -webkit-line-clamp: 3;
+              -webkit-box-orient: vertical;
+              overflow: hidden;
+              text-overflow: ellipsis;
+            }
+          }
+        }
+        .overlay-container:hover .overlay {
+          opacity: 1;
+        }
+      }
+    }
+    .row-2 {
+      display: flex;
+      width: 100%;
+      justify-content: space-between;
+      .gallery-item {
+        flex: 1;
+        margin: 0 5px;
+        img {
+          width: 100%;
+          height: auto;
+        }
+        .overlay-container {
+          position: relative;
+          overflow: hidden;
+          img {
+            display: block;
+            width: 100%;
+            height: auto;
+          }
+          .overlay {
+            @apply flex flex-col absolute top-0 left-0 p-4;
+            justify-content: flex-end;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 114, 171, 0.7);
+            opacity: 0;
+            transition: opacity 0.5s ease;
+            .title {
+              @apply text-start mb-1;
+              font-size: 24px; font-weight: 500; color: white;
+            }
+            .desc {
+              @apply text-white text-start font-normal;
+              font-size: 12px;
+              display: -webkit-box;
+              -webkit-line-clamp: 3;
+              -webkit-box-orient: vertical;
+              overflow: hidden;
+              text-overflow: ellipsis;
+            }
+          }
+        }
+        .overlay-container:hover .overlay {
+          opacity: 1;
+        }
+      }
+    }
+  }
+  .gallery-mob {
+    display: none;
   }
 }
 @media (max-width : 992px) {
   .main {
     padding: 0 0 48px;
-
     h2 {
-      font-size:28px
+      font-size: 24px;
+      margin-bottom: 12px;
     }
-
-    button {
+    .gallery {
+      @apply flex flex-col items-center;
+      .row-1 {
+        .gallery-item {
+          .overlay-container {
+            .overlay {
+              background: linear-gradient(to top, rgba(0, 114, 171, 0.8), transparent);
+              opacity: 1;
+              .title {
+                font-size: 24px;
+              }
+              .desc {
+                font-size: 14px;
+                -webkit-line-clamp: 3;
+              }
+            }
+          }
+          .overlay-container:hover .overlay {
+            opacity: 1;
+          }
+        }
+      }
+      .row-2 {
+        .gallery-item {
+          .overlay-container {
+            .overlay {
+              background: linear-gradient(to top, rgba(0, 114, 171, 0.8), transparent);
+              opacity: 1;
+              .title {
+                font-size: 20px;
+              }
+              .desc {
+                font-size: 10px;
+                -webkit-line-clamp: 2;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+@media (max-width : 992px) {
+  .main {
+    padding: 0 0 48px;
+    h2 {
+      font-size: 24px;
+      margin-bottom: 12px;
+    }
+    .gallery {
       display: none;
+    }
+    .gallery-mob {
+      @apply flex flex-col;
+      .carousel-image {
+        width: 100%;
+        height: auto;
+        border-radius: 10px;
+        margin-bottom: 10px;
+      }
+      .gallery-cont {
+        display: flex;
+        flex-direction: column;
+        .title {
+          font-size: 24px;
+          font-weight: 500;
+          margin: 10px 0;
+        }
+        .desc {
+          font-size: 14px;
+          line-height: 20px;
+          display: -webkit-box;
+          -webkit-line-clamp: 4;
+          -webkit-box-orient: vertical;
+          overflow: hidden;
+          text-overflow: ellipsis;
+        }
+      }
     }
   }
 }
